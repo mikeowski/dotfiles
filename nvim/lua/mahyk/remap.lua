30eea10f46aaa0abb7becd9a67373c9533f4d782
Burak Mike <burakmahyk@gmail.com>
2023-04-26 00:43:05 +0300

    fix: intersect keymap between harpoon and split switching

--- nvim/lua/mahyk/remap.lua
+++ nvim/lua/mahyk/remap.lua
@@ -4,11 +4,11 @@ vim.g.mapleader = " "
 -- line moving
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
--- better split switching
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
+-- -- better split switching
+-- vim.keymap.set("n", "<C-h>", "<C-w>h")
+-- vim.keymap.set("n", "<C-j>", "<C-w>j")
+-- vim.keymap.set("n", "<C-k>", "<C-w>k")
+-- vim.keymap.set("n", "<C-l>", "<C-w>l")
 
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")

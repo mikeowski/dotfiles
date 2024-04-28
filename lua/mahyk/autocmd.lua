27ed1dfbba5046842fc8b42d0f73ce589a53672c
Burak Mike <burakmahyk@gmail.com>
2024-04-29 00:35:56 +0300

    feat: nvim init

---
+++ lua/mahyk/autocmd.lua
@@ -0,0 +1,15 @@
+
+local augroup = vim.api.nvim_create_augroup   -- Create/get autocommand group
+local autocmd = vim.api.nvim_create_autocmd   -- Create autocommand
+
+
+
+-- Highlight on yank
+augroup('YankHighlight', { clear = true })
+autocmd('TextYankPost', {
+  group = 'YankHighlight',
+  callback = function()
+    vim.highlight.on_yank({ higroup = 'IncSearch', timeout = '1000' })
+  end
+})
+
---
+++ lua/mahyk/init.lua
@@ -0,0 +1,4 @@
+require("mahyk.remap")
+require("mahyk.set")
+require("mahyk.autocmd")
+require("mahyk.lazy_init")
